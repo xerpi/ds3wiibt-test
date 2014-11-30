576702deac4a10cf1a62f41441463831ac637e41
Sergi Granell <xerpi.g.12@gmail.com>
2014-11-30 14:28:32 +0100

    First commit

---
+++ source/ds3wiibt.c
@@ -0,0 +1,301 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <ogc/machine/processor.h>
+#include <bte/bte.h>
+#include "ds3wiibt.h"
+#include "hci.h"
+#include "btpbuf.h"
+
+static int senddata_raw(struct l2cap_pcb *pcb, const void *message, u16 len);
+int set_operational(struct ds3wiibt_context *ctx);
+static int send_output_report(struct ds3wiibt_context *ctx);
+static void correct_input(struct ds3wiibt_input *inp);
+
+static err_t l2ca_connect_ind_cb(void *arg, struct l2cap_pcb *pcb, err_t err);
+
+static err_t l2ca_disconnect_ind_cb(void *arg, struct l2cap_pcb *pcb, err_t err);
+static err_t l2ca_disconnect_cfm_cb(void *arg, struct l2cap_pcb *pcb);
+static err_t l2ca_timeout_ind_cb(void *arg, struct l2cap_pcb *pcb, err_t err);
+static err_t l2ca_recv_cb(void *arg, struct l2cap_pcb *pcb, struct pbuf *p, err_t err);
+
+void ds3wiibt_initialize(struct ds3wiibt_context *ctx)
+{
+	ctx->ctrl_pcb = NULL;
+	ctx->data_pcb = NULL;
+	memset(&ctx->bdaddr, 0, sizeof(ctx->bdaddr));
+	memset(&ctx->input,  0, sizeof(ctx->input));
+	ds3wiibt_set_led(ctx, 0x02);
+	ds3wiibt_set_rumble(ctx, 0x00, 0x00, 0x00, 0x00);
+	ctx->usrdata = NULL;
+	ctx->connect_cb = NULL;
+	ctx->disconnect_cb = NULL;
+	ctx->status = DS3WIIBT_STATUS_DISCONNECTED;
+}
+
+void ds3wiibt_set_user_data(struct ds3wiibt_context *ctx, void *data)
+{
+	ctx->usrdata = data;
+}
+
+void ds3wiibt_set_connect_cb(struct ds3wiibt_context *ctx, ds3wiibt_cb cb)
+{
+	ctx->connect_cb = cb;
+}
+
+void ds3wiibt_set_disconnect_cb(struct ds3wiibt_context *ctx, ds3wiibt_cb cb)
+{
+	ctx->disconnect_cb = cb;
+}
+
+void ds3wiibt_set_led(struct ds3wiibt_context *ctx, u8 led)
+{
+	ctx->led = led;
+}
+
+void ds3wiibt_set_rumble(struct ds3wiibt_context *ctx, u8 duration_right, u8 power_right, u8 duration_left, u8 power_left)
+{
+	ctx->rumble.duration_right = duration_right;
+	ctx->rumble.power_right = power_right;
+	ctx->rumble.duration_left = duration_left;
+	ctx->rumble.power_left = power_left;
+}
+
+void ds3wiibt_send_ledsrumble(struct ds3wiibt_context *ctx)
+{
+	send_output_report(ctx);
+}
+
+void ds3wiibt_connect(struct ds3wiibt_context *ctx, struct bd_addr *addr)
+{
+	u32 level = IRQ_Disable();
+	
+	bd_addr_set(&ctx->bdaddr, addr);
+
+	ctx->ctrl_pcb = l2cap_new();
+	ctx->data_pcb = l2cap_new();
+	
+	l2cap_arg(ctx->ctrl_pcb, ctx);
+	l2cap_arg(ctx->data_pcb, ctx);
+	
+	l2cap_disconnect_ind(ctx->ctrl_pcb, l2ca_disconnect_ind_cb);
+	l2cap_disconnect_ind(ctx->data_pcb, l2ca_disconnect_ind_cb);
+	
+	l2cap_timeout_ind(ctx->ctrl_pcb, l2ca_timeout_ind_cb);
+	l2cap_timeout_ind(ctx->data_pcb, l2ca_timeout_ind_cb);
+	
+	l2cap_connect_ind(ctx->ctrl_pcb,  &ctx->bdaddr, HIDP_PSM, l2ca_connect_ind_cb);
+	l2cap_connect_ind(ctx->data_pcb,  &ctx->bdaddr, INTR_PSM, l2ca_connect_ind_cb);
+
+	IRQ_Restore(level);
+}
+
+void ds3wiibt_close(struct ds3wiibt_context *ctx)
+{
+	//Turn OFF the LED and Rumble
+	ds3wiibt_set_led(ctx, 0);
+	ds3wiibt_set_rumble(ctx, 0x00, 0x00, 0x00, 0x00);
+	ds3wiibt_send_ledsrumble(ctx);
+	if (is_connected(ctx)) {
+		if (ctx->ctrl_pcb) l2ca_disconnect_req(ctx->ctrl_pcb, l2ca_disconnect_cfm_cb);
+		if (ctx->data_pcb) l2ca_disconnect_req(ctx->data_pcb, l2ca_disconnect_cfm_cb);
+	} else {
+		if (ctx->ctrl_pcb) l2cap_close(ctx->ctrl_pcb);
+		if (ctx->data_pcb) l2cap_close(ctx->data_pcb);
+		ctx->ctrl_pcb = NULL;
+		ctx->data_pcb = NULL;
+	}
+	while (is_connected(ctx)) usleep(50);
+}
+
+static err_t l2ca_recv_cb(void *arg, struct l2cap_pcb *pcb, struct pbuf *p, err_t err)
+{
+	struct ds3wiibt_context *ctx = (struct ds3wiibt_context *)arg;
+	if (ctx == NULL || pcb == NULL || err != ERR_OK) return -1;
+
+	u8 *rd = p->payload;
+	
+	//LOG("RECV, PSM: 0x%02X  len: %i |", l2cap_psm(pcb), p->len);
+	//LOG(" 0x%X  0x%X  0x%X  0x%X  0x%X\n", rd[0], rd[1], rd[2], rd[3], rd[4]);
+	
+	switch (l2cap_psm(pcb))	{
+	case HIDP_PSM:
+		break;
+	case INTR_PSM:
+		switch (rd[1]) {
+		case 0x01: //Full report
+			memcpy(&ctx->input, rd + 1, sizeof(ctx->input));
+			break;
+		}
+		break;
+	}
+
+	return ERR_OK;
+}
+
+static err_t l2ca_connect_ind_cb(void *arg, struct l2cap_pcb *pcb, err_t err)
+{
+	struct ds3wiibt_context *ctx = (struct ds3wiibt_context *)arg;
+	if (ctx == NULL || pcb == NULL) return -1;
+	
+	LOG("l2ca_connect_ind_cb, PSM: 0x%02X\n", l2cap_psm(pcb));
+	
+	if (l2cap_psm(pcb) == HIDP_PSM) {
+		/* Control PSM is connected */
+		LOG("Waiting for a DATA connection request ..\n");
+		l2cap_recv(ctx->ctrl_pcb, l2ca_recv_cb);
+	} else if (l2cap_psm(pcb) == INTR_PSM) {
+		LOG("Got DATA connection!\n");
+		LOG("Set operational\n");
+		//usleep(200*1000);
+		//set_operational(ctx);
+		LOG("Output Report\n");
+		//usleep(200*1000);
+		//send_output_report(ctx);
+		l2cap_recv(ctx->data_pcb, l2ca_recv_cb);
+		
+		ctx->status = DS3WIIBT_STATUS_CONNECTED;
+		if (ctx->connect_cb != NULL) {
+			//Notify the user
+			ctx->connect_cb(ctx->usrdata);
+		}
+	}
+	
+	return ERR_OK;
+}
+
+static err_t l2ca_disconnect_ind_cb(void *arg, struct l2cap_pcb *pcb, err_t err)
+{
+	struct ds3wiibt_context *ctx = (struct ds3wiibt_context *)arg;
+	if (ctx == NULL || pcb == NULL) return -1;
+	
+	LOG("l2ca_disconnect_ind_cb, PSM: 0x%02X\n", l2cap_psm(pcb));
+	
+	switch (l2cap_psm(pcb))	{
+	case HIDP_PSM:
+		l2cap_close(ctx->ctrl_pcb);
+		ctx->ctrl_pcb = NULL;
+		break;
+	case INTR_PSM:
+		l2cap_close(ctx->data_pcb);
+		ctx->data_pcb = NULL;
+		break;
+	}
+	
+	//If the Data and Control PSMs are disconnected, listen for an incoming connection again
+	if ((ctx->ctrl_pcb == NULL) && (ctx->data_pcb == NULL)) {
+		
+		ctx->status = DS3WIIBT_STATUS_DISCONNECTED;
+		if (ctx->disconnect_cb != NULL) ctx->disconnect_cb(ctx->usrdata);
+		
+		ctx->ctrl_pcb = l2cap_new();
+		ctx->data_pcb = l2cap_new();
+		
+		l2cap_arg(ctx->ctrl_pcb, ctx);
+		l2cap_arg(ctx->data_pcb, ctx);
+		
+		l2cap_disconnect_ind(ctx->ctrl_pcb, l2ca_disconnect_ind_cb);
+		l2cap_disconnect_ind(ctx->data_pcb, l2ca_disconnect_ind_cb);
+		
+		l2cap_timeout_ind(ctx->ctrl_pcb, l2ca_timeout_ind_cb);
+		l2cap_timeout_ind(ctx->data_pcb, l2ca_timeout_ind_cb);
+
+		l2cap_recv(ctx->ctrl_pcb, l2ca_recv_cb);
+		l2cap_recv(ctx->data_pcb, l2ca_recv_cb);
+	}
+
+	return ERR_OK;
+}
+
+static err_t l2ca_disconnect_cfm_cb(void *arg, struct l2cap_pcb *pcb)
+{
+	struct ds3wiibt_context *ctx = (struct ds3wiibt_context *)arg;
+	if (ctx == NULL || pcb == NULL) return -1;
+	
+	LOG("l2ca_disconnect_cfm_cb, PSM: 0x%02X\n", l2cap_psm(pcb));
+	
+	switch (l2cap_psm(pcb))	{
+	case HIDP_PSM:
+		l2cap_close(ctx->ctrl_pcb);
+		ctx->ctrl_pcb = NULL;
+		break;
+	case INTR_PSM:
+		l2cap_close(ctx->data_pcb);
+		ctx->data_pcb = NULL;
+		break;
+	}
+	//User has finished it, so don't listen again
+	if ((ctx->ctrl_pcb == NULL) && (ctx->data_pcb == NULL)) {
+		ctx->status = DS3WIIBT_STATUS_DISCONNECTED;
+		if (ctx->disconnect_cb != NULL) ctx->disconnect_cb(ctx->usrdata);
+	}
+
+	return ERR_OK;
+}
+
+static err_t l2ca_timeout_ind_cb(void *arg, struct l2cap_pcb *pcb, err_t err)
+{
+	struct ds3wiibt_context *ctx = (struct ds3wiibt_context *)arg;
+	if (ctx == NULL || pcb == NULL) return -1;
+	
+	LOG("l2ca_timeout_ind_cb, PSM: 0x%02X\n", l2cap_psm(pcb));
+
+	//Disconnect?
+	switch (l2cap_psm(pcb))	{
+	case HIDP_PSM:
+		break;
+	case INTR_PSM:
+		break;
+	}
+
+	return ERR_OK;
+}
+
+
+static int senddata_raw(struct l2cap_pcb *pcb, const void *message, u16 len)
+{
+	err_t err;
+	struct pbuf *p;
+	if ((pcb == NULL) || (message==NULL) || (len==0)) return ERR_VAL;
+	if ((p = btpbuf_alloc(PBUF_RAW, (len), PBUF_RAM)) == NULL) {
+		return ERR_MEM;
+	}
+	memcpy(p->payload, message, len);
+	err = l2ca_datawrite(pcb, p);
+	btpbuf_free(p);
+	return err;
+}
+
+int set_operational(struct ds3wiibt_context *ctx)
+{
+	static const unsigned char buf[] ATTRIBUTE_ALIGN(32) = {
+		(HIDP_TRANS_SETREPORT | HIDP_DATA_RTYPE_FEATURE),
+		0xF4, 0x42, 0x03, 0x00, 0x00
+	};
+	return senddata_raw(ctx->ctrl_pcb, buf, sizeof(buf));
+}
+
+static int send_output_report(struct ds3wiibt_context *ctx)
+{
+	unsigned char buf[] ATTRIBUTE_ALIGN(32) = {
+		(HIDP_TRANS_SETREPORT | HIDP_DATA_RTYPE_OUPUT),
+		0x01, //Report ID
+		0x00, //Padding
+		0x00, 0x00, 0x00, 0x00, //Rumble (r, r, l, l)
+		0x00, 0x00, 0x00, 0x00, //Padding
+		0x00, /* LED_1 = 0x02, LED_2 = 0x04, ... */
+		0xff, 0x27, 0x10, 0x00, 0x32, /* LED_4 */
+		0xff, 0x27, 0x10, 0x00, 0x32, /* LED_3 */
+		0xff, 0x27, 0x10, 0x00, 0x32, /* LED_2 */
+		0xff, 0x27, 0x10, 0x00, 0x32, /* LED_1 */
+		0x00, 0x00, 0x00, 0x00, 0x00  /* LED_5 (not soldered) */
+	};
+	
+	buf[3] = ctx->rumble.duration_right;
+	buf[4] = ctx->rumble.power_right;
+	buf[5] = ctx->rumble.duration_left;
+	buf[6] = ctx->rumble.power_left;
+	buf[11] = ctx->led;
+
+	return senddata_raw(ctx->ctrl_pcb, buf, sizeof(buf));
+}
+
---
+++ source/main.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <gccore.h>
+#include <ogc/machine/processor.h>
+#include <fat.h>
+#include <wiiuse/wpad.h>
+#include "ds3wiibt.h"
+#include "utils.h"
+
+//Controller's MAC: 00:24:33:63:4B:6B
+struct bd_addr addr = {.addr = {0x6B, 0x4B, 0x63, 0x33, 0x24, 0x00}};
+//struct bd_addr addr = {.addr = {0x00, 0x24, 0x33, 0x63, 0x4B, 0x6B}};
+
+static void print_data(struct ds3wiibt_input *inp);
+static void conn_cb(void *usrdata);
+static void discon_cb(void *usrdata);
+
+int main(int argc, char *argv[])
+{
+	fatInitDefault();
+	WPAD_Init();
+	init_video();
+	SYS_SetResetCallback(button_cb);
+	SYS_SetPowerCallback(button_cb);
+	printf("ds3wiibt by xerpi\n");
+
+
+	struct ds3wiibt_context ctx;
+	ds3wiibt_initialize(&ctx);
+	ds3wiibt_set_user_data(&ctx, NULL);
+	ds3wiibt_set_connect_cb(&ctx, conn_cb);
+	ds3wiibt_set_disconnect_cb(&ctx, discon_cb);
+
+	LOG("Connecting to: ");
+	print_mac((struct bd_addr*)addr.addr);
+	ds3wiibt_connect(&ctx, &addr);
+	printf("Listening for an incoming connection...\n");
+	
+	int inited = 0;
+	
+	while (run) {
+		WPAD_ScanPads();
+		u32 pressed = WPAD_ButtonsDown(0);
+		if (pressed & WPAD_BUTTON_HOME) run = 0;
+		if (is_connected(&ctx)) {
+			if (!inited) {
+				usleep(200*1000);
+				set_operational(&ctx);
+				usleep(200*1000);
+				ds3wiibt_send_ledsrumble(&ctx);
+				inited = 1;
+			}
+			print_data(&ctx.input);
+			if (ctx.input.PS && ctx.input.start) run = 0;
+		}
+		flip_screen();
+	}
+	//ds3wiibt_close(&ctx);
+	return 0;
+}
+
+static void conn_cb(void *usrdata)
+{
+	printf("Controller connected.\n");
+}
+
+static void discon_cb(void *usrdata)
+{
+	printf("Controller disconnected.\n");
+}
+
+static void print_data(struct ds3wiibt_input *inp)
+{
+	printf("\x1b[10;0H");
+	printf("\n\nPS: %i   START: %i   SELECT: %i   /\\: %i   []: %i   O: %i   X: %i   L3: %i   R3: %i\n", \
+		inp->PS, inp->start, inp->select, inp->triangle, \
+		inp->square, inp->circle, inp->cross, inp->L3, inp->R3);
+
+	printf("L1: %i   L2: %i   R1: %i   R2: %i   UP: %i   DOWN: %i   RIGHT: %i   LEFT: %i\n", \
+		inp->L1, inp->L2, inp->R1, inp->R2, \
+		inp->up, inp->down, inp->right, inp->left);
+
+	printf("LX: 0x%02X   LY: 0x%02X   RX: 0x%02X   RY: 0x%02X\n", \
+		inp->leftX, inp->leftY, inp->rightX, inp->rightY);
+
+	printf("aX: 0x%04X   aY: 0x%04X   aZ: 0x%04X   Zgyro: 0x%04X\n", \
+		inp->accelX, inp->accelY, inp->accelZ, inp->gyroZ);
+
+	printf("L1 sens: 0x%02X   L2 sens: 0x%02X   R1 sens: 0x%02X   R2 sens: 0x%02X\n", \
+		inp->L1_sens, inp->L2_sens, inp->R1_sens, inp->R2_sens);
+
+	printf("/\\ sens: 0x%02X   [] sens: 0x%02X   O sens: 0x%02X   X sens: 0x%02X\n",
+		inp->triangle_sens, inp->square_sens, inp->circle_sens, inp->cross_sens);
+
+	printf("UP: 0x%02X   DOWN: 0x%02X   RIGHT: 0x%02X   LEFT: 0x%02X\n", \
+		inp->up_sens, inp->down_sens, inp->right_sens, inp->left_sens);
+}
